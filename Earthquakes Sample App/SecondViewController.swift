165357bd5aa61436c383a8e864a8e3b182d84275
Ali Afghah <ali_afghah@mac.com>
2016-05-07 10:06:03 +0430

    Initial Commit

---
+++ Earthquakes Sample App/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Earthquakes Sample App
+//
+//  Created by Ali Afghah on 5/7/16.
+//  Copyright © 2016 Ali Afghah. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
